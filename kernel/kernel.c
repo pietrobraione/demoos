7aa6df0952c33cf574da19b66f76ce4b6301d5db
Carloa <c.angrisani1@campus.unimib.it>
2025-10-27 11:28:49 +0100

    [bugfix/remove-broken-framebuffer] Rimosso blocco codice framebuffer non funzionante

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,13 +7,6 @@ void kernel_main(uint64_t dtb_ptr32, uint64_t x1, uint64_t x2, uint64_t x3)
 {
   uart_init();
   uart_puts("Hello, kernel world!\r\n");
-
-  int ok = framebuffer_init();
-  if (ok) {
-    framebuffer_print(0, 0, "Hello, framebuffer world!\n");
-  } else {
-    uart_puts("Initialization of framebuffer failed.\r\n");
-  }
   
   while (1)
     uart_putc(uart_getc());
